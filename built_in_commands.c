ddc46720879735daa8c147842e44316f82e56090
MrtnOmwenga <martin36449@gmail.com>
2023-06-23 10:22:48 -0700

    Added documentation

--- built_in_commands.c
+++ built_in_commands.c
@@ -1,23 +1,43 @@
 #include "shell.h"
 
+/**
+ * own_cd- Change directory
+ *
+ * Return: 1
+ */
 int own_cd(void)
 {
 printf("Working on it... \n");
 return (0);
 }
 
+/**
+ * own_env- Returns environment variables
+ *
+ * Return: 1
+ */
 int own_env(void)
 {
 printf("Working on it ... \n");
 return (0);
 }
 
+/**
+ * own_help- Help
+ *
+ * Return: 1
+ */
 int own_help(void)
 {
 printf("Working on it ... \n");
 return (0);
 }
 
+/**
+ * own_exit- Exit shell
+ *
+ * Return: 1
+ */
 int own_exit(void)
 {
 printf("Working on it... \n");
