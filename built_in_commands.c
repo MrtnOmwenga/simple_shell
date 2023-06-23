6e5ae2c0921e1c89c0b66c826ae4cb59b2057160
MrtnOmwenga <martin36449@gmail.com>
2023-06-23 09:59:36 -0700

    Simple shell

---
+++ built_in_commands.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+int own_cd() {
+  printf("Working on it... \n");
+  return (0);
+}
+
+int own_env() {
+  printf("Working on it ... \n");
+  return (0);
+}
+
+int own_help() {
+  printf("Working on it ... \n");
+  return (0);
+}
+
+int own_exit() {
+  printf("Working on it... \n");
+  return (0);
+}
