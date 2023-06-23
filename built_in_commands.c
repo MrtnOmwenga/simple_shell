e6e083a6c97dc7f87071cf29b44202bb2aa7a6b1
MrtnOmwenga <martin36449@gmail.com>
2023-06-23 10:45:43 -0700

    Edited exit function to exit(0)

--- built_in_commands.c
+++ built_in_commands.c
@@ -29,7 +29,16 @@ return (0);
  */
 int own_help(void)
 {
-printf("Working on it ... \n");
+puts("\n***WELCOME TO MY SHELL HELP***"
+        "\nCopyright @ Open Source"
+        "\n-Use the shell at your own risk..."
+        "\nList of Commands supported:"
+        "\n>cd"
+        "\n>ls"
+        "\n>exit"
+        "\n>all other general commands available in UNIX shell"
+        "\n>pipe handling"
+        "\n>improper space handling");
 return (0);
 }
 
@@ -40,5 +49,5 @@ return (0);
  */
 int own_exit(void)
 {
-return (0);
+exit(0);
 }
